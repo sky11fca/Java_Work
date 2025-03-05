7592c4d7e2837b08e2d2b216f64565ddf1e00c25
Bazon Bogdan <152993164+sky11fca@users.noreply.github.com>
2025-03-05 17:21:38 +0200

    WEEK 2

--- W2/src/main/java/projectType.java
+++ W2/src/main/java/projectType.java
@@ -1,5 +1,6 @@
 public enum projectType
 {
+    //enum types
     PRACTICAL,
     THEORETICAL;
 }
---
+++ W2/src/main/java/studentInstance.java
@@ -0,0 +1,36 @@
+public class studentInstance {
+    private students student;
+    private project project;
+
+    public studentInstance(students student, project project)
+    {
+        this.student = student;
+        this.project = project;
+    }
+
+    public studentInstance() {}
+
+    public students getStudent() {
+        return student;
+    }
+
+    public void setStudent(students student) {
+        this.student = student;
+    }
+
+    public project getProject() {
+        return project;
+    }
+
+    public void setProject(project project) {
+        this.project = project;
+    }
+
+    @Override
+    public String toString() {
+        return "studentInstance{" +
+                "student=" + student.toString() +
+                ", project=" + project.toString() +
+                '}';
+    }
+}
